8c2f1445addf3a95ba8239552e854ffd7bda63a5
ArupDas15 <das12997@gmail.com>
2021-03-26 20:40:18 +0530

    Fix for RuntimeError: You must compile your model before using it in main.py. Reference: https://www.programmersought.com/article/39741465652/

--- partA/main.py
+++ partA/main.py
@@ -1,28 +1,36 @@
+#used to initialize NN
 from keras.models import Sequential
-# import os
-# print(os.listdir("../iNaturalist_Dataset/inaturalist_12K/train/"))
-from keras.datasets import fashion_mnist
+#used for convulotion step which makes convolution layer
 from keras.layers.convolutional import Conv2D
-from keras.layers import Dense, Flatten, InputLayer
+# used to add layers (hidden layer and outputs)
+from keras.layers import Dense
+# used to convert pooled features into vector that will be input to NN
+from keras.layers import Flatten
+# used for pooling step which makes pooling layer
 from keras.layers.convolutional import MaxPooling2D
 from keras.layers import Activation
 import tensorflow as tf
-import numpy as np
-from data import *
+from partA.data import *
 
 #placeholder - here I will get the data
 train = preprocess_img(datatype='train',batch_size=32,target_size=(240,240))
 validate=preprocess_img(datatype='validate',batch_size=32,target_size=(240,240))
 
-#creating objet of sequential model
+#creating an object of sequential model
 model = Sequential()
 
 #this function builds the CNN with parameters passed by the users.
-def build(filters,size_filters,neurons,activation):
+def build(filters,size_filters,neurons,activation,input_shape):
+  # Boolean flag to check if it is the first convolution layer
+  first_conv_layer = 0
   # iterating through filter and size_filters taking one from each at a time
   for (f,s) in zip(filters,size_filters):
-    #Adding convulational layer with f filters and s as the poolsize
-    model.add(Conv2D(f, s))
+    # Adding convulational layer with f filters and s as the poolsize
+    if first_conv_layer == 0:
+      model.add(Conv2D(filters=f,kernel_size=s,input_shape=input_shape))
+      first_conv_layer = first_conv_layer + 1
+    else:
+      model.add(Conv2D(filters=f,kernel_size=s))
     #adding relu layer after convulational layer
     model.add(Activation(activation))
     #adding max pooling layer
@@ -33,11 +41,10 @@ def build(filters,size_filters,neurons,activation):
   model.add(Flatten())
   #output layer
   model.add(Dense(10,activation='softmax'))
-  #compiling the whole thing
   model.compile(optimizer=tf.keras.optimizers.Adam(), loss='categorical_crossentropy',metrics=["accuracy"])
 
 
-build([16,16,16,16,16],[(3,3),(3,3),(3,3),(3,3),(3,3)],10,'relu')
+build([16,16,16,16,16],[(3,3),(3,3),(3,3),(3,3),(3,3)],10,'relu',input_shape=(240,240,3))
 #training
 model.fit( train,
         steps_per_epoch=len(train),
