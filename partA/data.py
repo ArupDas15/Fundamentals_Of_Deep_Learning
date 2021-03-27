8d484eb2c7f54186181644fdb8f5e42fdcdad2e4
Utsav Dey <deyyutsav@gmail.com>
2021-03-27 14:39:10 +0530

    Made data.py more robust.

--- partA/data.py
+++ partA/data.py
@@ -5,25 +5,31 @@ import glob
 import numpy as np
 from os import path
 
+base_path = '/content/iNaturalist_Dataset/inaturalist_12K/'
+
 
 # Reference: https://jdhao.github.io/2017/11/06/resize-image-to-square-with-padding/
 def resize(datatype, target_size=(240, 240)):
     desired_size = target_size[0]
-    base_path = os.path.join(os.path.dirname(os.getcwd()), 'iNaturalist_Dataset', 'inaturalist_12K', datatype)
     # Get the list of folders in the directory datatype
-    class_labels = os.listdir(base_path)
+    datatype_path = os.path.join(base_path, datatype)
+    output_path = os.path.join(base_path, 'Output')
+    class_labels = os.listdir(datatype_path)
     # Assigns a numerical value to every distinct class in the dataset by creating a dictionary.
-    label_dict = {class_labels[i]: class_labels.index(class_labels[i]) for i in range(0, len(class_labels))}
-    # data is a list which stores the numpy array of every image
+    label_dict = {'Amphibia': 0, 'Reptilia': 1, 'Plantae': 2, 'Mollusca': 3, 'Fungi': 4, 'Aves': 5, 'Mammalia': 6,
+                  'Animalia': 7, 'Insecta': 8,
+                  'Arachnida': 9}  # data is a list which stores the numpy array of every image
+    # store the numpy data
     data = []
     # data_label is a list which stores the categorical label corresponding to every image matrix in data
     data_label = []
     for key, value in label_dict.items():
-        class_folder = base_path + os.sep + key + os.sep
-        if datatype == 'train':
-            os.makedirs(os.path.join(os.path.dirname(os.getcwd()), 'Output', datatype, key))
-            i = 1
-        for file in glob.glob(class_folder + "*"):
+        class_folder = os.path.join(datatype_path, key)
+        if datatype in ('train', 'validate'):
+            if not path.exists(os.path.join(output_path, datatype, key)):
+                os.makedirs(os.path.join(output_path, datatype, key))
+
+        for file in glob.glob(class_folder + "/*"):
             im = Image.open(file)
             current_size = im.size
             # We divide by the maximum dimension so that the resized image does not extend beyond the desired size
@@ -40,40 +46,48 @@ def resize(datatype, target_size=(240, 240)):
             floor. We are doing this there should be equal amount of margin on both sides."""
             new_im.paste(im, ((desired_size - new_size[0]) // 2,
                               (desired_size - new_size[1]) // 2))
-            if datatype == 'train':
-                file_name = os.path.join(os.path.dirname(os.getcwd()), 'Output', datatype, key, str(i) + '.jpg')
+            if datatype in ('train', 'validate'):
+                file_name = os.path.join(output_path, datatype, key, os.path.basename(file))
                 new_im.save(file_name)
-                i = i + 1
-            img = np.array(new_im)
-            data.append(img)
-            data_label.append(value)
-    if datatype != 'train':
+            else:
+                img = np.array(new_im)
+                data.append(img)
+                data_label.append(value)
+    if datatype not in ('train', 'validate'):
         return np.array(data), np.array(data_label)
 
 
-# Reference: https://www.tensorflow.org/api_docs/python/tf/keras/preprocessing/image/ImageDataGenerator#flow_from_directory
+# Reference: https://www.tensorflow.org/api_docs/python/tf/keras/preprocessing/image/ImageDataGenerator
+# #flow_from_directory
 def preprocess_img(datatype, batch_size=64, target_size=(240, 240)):
-    if datatype != 'train':
-        re_scaler = ImageDataGenerator(rescale=1. / 255)
+    data = None
+    output_path = os.path.join(base_path, 'Output')
+    datatype_path = os.path.join(output_path, datatype)
+    re_scale = ImageDataGenerator(rescale=1. / 255)
+    dataset_augment = ImageDataGenerator(rescale=1. / 255, rotation_range=30,
+                                         width_shift_range=0.3,
+                                         height_shift_range=0.2,
+                                         shear_range=0.1,
+                                         zoom_range=0.3)
+
+    if datatype == 'test':
+
         # x is a numpy array of image data and y is a numpy array of corresponding labels.
         x, y = resize(datatype, target_size=target_size)
         # load and iterate over validation or test data.
-        data = re_scaler.flow(x, y, batch_size=batch_size)
-    else:
-        """ Since the training data is large in size so we save the processed images in Output folder before doing any further computations.
-            This workaround is done because on the fly computation could lead to memory error due to large training data. 
-        """
-        dataset_augmentar = ImageDataGenerator(rescale=1. / 255, rotation_range=30,
-                                               width_shift_range=0.3,
-                                               height_shift_range=0.2,
-                                               shear_range=0.1,
-                                               zoom_range=0.3)
-        """ if Output data is available don't resize again. If it's not
-         available use training data to get it's resized version."""
-        if not path.exists(os.path.join(os.path.dirname(os.getcwd()), 'Output', datatype)):
+        data = re_scale.flow(x, y, batch_size=batch_size)
+    elif datatype == 'validate':
+        """Since the training data is large in size so we save the processed images in Output folder before doing any 
+        further computations. This workaround is done because on the fly computation could lead to memory error due 
+        to large training data. """
+        if not path.exists(datatype_path):
             resize(datatype, target_size=target_size)
         # load and iterate over training dataset. To evaluate the model set ‘shuffle‘ to ‘False.’
-        data = dataset_augmentar.flow_from_directory(os.path.join(os.path.dirname(os.getcwd()), 'Output', datatype),
-                                                     class_mode='categorical', batch_size=batch_size,
-                                                     shuffle=True)
+        data = re_scale.flow_from_directory(os.path.join(output_path, datatype), shuffle=False)
+    elif datatype == 'train':
+        print('datatype')
+        if not path.exists(datatype_path):
+            resize(datatype, target_size=target_size)
+            # load and iterate over training dataset. To evaluate the model set ‘shuffle‘ to ‘False.’
+        data = dataset_augment.flow_from_directory(os.path.join(output_path, datatype), shuffle=True)
     return data
