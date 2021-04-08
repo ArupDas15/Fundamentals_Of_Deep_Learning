c0121889d8eb8bc351e33ecf8712ad95b36068c3
Utsav Dey <deyyutsav@gmail.com>
2021-04-08 15:20:21 +0530

    Revert "Wand B Support Added"
    This reverts commit 163e7ae4c4b97367db8c6198f514a5c4e44f086a.

--- partA/main.py
+++ partA/main.py
@@ -1,16 +1,17 @@
 from keras.models import Sequential
 from keras_preprocessing.image import ImageDataGenerator
+# import os
+# print(os.listdir("../iNaturalist_Dataset/inaturalist_12K/train/"))
+from keras.datasets import fashion_mnist
 from keras.layers.convolutional import Conv2D
 from keras.layers import Dense, Flatten, InputLayer
 from keras.layers.convolutional import MaxPooling2D
 from keras.layers import Activation
 import tensorflow as tf
-import wandb
-from wandb.keras import WandbCallback
+import numpy as np
 
 
 
-path='/content/iNaturalist_Dataset/inaturalist_12K/'
 
 
 # this function builds the CNN with parameters passed by the users.
@@ -47,33 +48,3 @@ def build(input_shape, filters, size_filters, neurons, activation, optimiser, no
     # compiling the whole thing
     model.compile(optimizer=optimiser, loss='categorical_crossentropy', metrics=["accuracy"])
     return model
-
-def train():
-    run = wandb.init()
-    opti = None
-    #wandb.run.name = run.config.optimiser+str(np.random.rand(1))
-    if run.config.filter_architecture == 'increasing':
-      filter_architecture=[64,128,256,350,380]
-    elif run.config.filter_architecture == 'decreasing':
-      filter_architecture=[64,70,80,90,256]
-    elif run.config.filter_architecture == 'equal':
-      filter_architecture= [64,70,80,90,128]
-    print(filter_architecture)
-    optimiser = tf.keras.optimizers.Nadam(learning_rate=.0007)
-    normalize=False
-    if run.config.batch_normalize == 'Yes':
-        normalize=True
-    dropout =False
-    dropout_rate=0
-    if run.config.dropout != 'None':
-      dropout_rate=run.config.dropout
-      dropout=True
-    model=build((299,299,3),filter_architecture,[(3,3),(3,3),(3,3),(3,3),(3,3)],32,'relu',optimiser=optimiser,normalize=normalize,dropout=dropout,dropout_rate=dropout_rate)
-    #training
-    dataset_augment = ImageDataGenerator(rescale=1. / 255,rotation_range=20, width_shift_range=0.1, height_shift_range=0.1,
-                         shear_range=0.2, zoom_range=0.2, horizontal_flip=True,
-                         fill_mode="nearest")
-    train=dataset_augment.flow_from_directory(os.path.join(path,'train'), shuffle=True, target_size=(600,600),batch_size=32)
-    validate=ImageDataGenerator(rescale=1. / 255).flow_from_directory(os.path.join(path,'validate'), shuffle=False, target_size=(600,600))
-    model.fit(train ,steps_per_epoch=len(train),epochs=10, validation_data=validate,callbacks=[WandbCallback()])
-    model.save('/content/drive/MyDrive/Models/model.keras')
